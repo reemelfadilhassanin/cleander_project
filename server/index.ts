e0fae6722d6a4f750f33061670b61dea9099ab0d
reemelfadilhassanin <justreemjust49@gmail.com>
2025-05-29 23:53:43 +0200

    age calcluter

--- server/index.ts
+++ server/index.ts
@@ -19,6 +19,18 @@ const allowedOrigins = [
 ];
 
 console.log('✅ Allowed CORS Origins:', allowedOrigins);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://cleander-project-front.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '86400'); // 24 ساعة
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 
 // ✅ CORS middleware
 app.use(
